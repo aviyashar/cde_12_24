6eea687dcc19a2e430f48cb52dadb0043924e573
aviyashar <avi131293@gmail.com>
2025-04-28 20:27:33 +0300

    Data check2

--- tests/test_data_pipeline.py
+++ tests/test_data_pipeline.py
@@ -21,7 +21,28 @@ class TestDataPipeline(unittest.TestCase):
         processed_data = pd.read_csv(output_file)
 
         # Check if values are doubled
-        expected_data = {'id': [1, 2, 3], 'value': [20, 40, 50]}
+        expected_data = {'id': [1, 2, 3], 'value': [20, 40, 60]}
+        expected_df = pd.DataFrame(expected_data)
+        pd.testing.assert_frame_equal(processed_data, expected_df)
+
+    def test_process_data_two(self):
+        # Create a temporary DataFrame for testing
+        data = {'id': [1, 2, 3], 'value': [10, 20, 30]}
+        df = pd.DataFrame(data)
+
+        # Save DataFrame to CSV
+        input_file = 'test_input_data.csv'
+        df.to_csv(input_file, index=False)
+
+        # Process the data
+        output_file = 'test_output_data.csv'
+        process_data(input_file, output_file)
+
+        # Load processed data
+        processed_data = pd.read_csv(output_file)
+
+        # Check if values are doubled
+        expected_data = {'id': [1, 2, 3], 'value': [20, 40, 60]}
         expected_df = pd.DataFrame(expected_data)
         pd.testing.assert_frame_equal(processed_data, expected_df)
 
